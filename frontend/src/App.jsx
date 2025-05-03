d93ba1184781110fa81c44c0d3d34b9e9beb3800
Monil501 <monilmodi501@gmail.com>
2025-05-03 10:31:51 +0530

    captain signup and login done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,45 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home.jsx'
 import UserLogin from './pages/UserLogin.jsx'
 import UserSignUp from './pages/UserSignUp.jsx'
 import CaptainLogin from './pages/CaptainLogin.jsx'
 import CaptainSignup from './pages/CaptainSignup.jsx'
+import Start from './pages/Start.jsx'
+import Home from './pages/Home.jsx'
+import UserProtectWrapper from './pages/UserProtectWrapper.jsx'
+import UserLogout from './pages/UserLogout.jsx'
+import CaptainHome from './pages/CaptainHome.jsx'
+import CaptainProtectWrapper from './pages/CaptainProtectWrapper.jsx'
 
 const App = () => {
 
   return (
     <div>
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/' element={<Start />} />
         <Route path='/login' element={<UserLogin />} />
         <Route path='/signup' element={<UserSignUp />} />
         <Route path='/captain-login' element={<CaptainLogin />} />
         <Route path='/captain-signup' element={<CaptainSignup />} />
+        <Route path='/home'
+          element={
+            <UserProtectWrapper>
+              <Home />
+            </UserProtectWrapper>
+          } />
+
+          <Route path='/user/logout'
+          element={<UserProtectWrapper>
+            <UserLogout />
+          </UserProtectWrapper>
+          } />
+
+<Route path='/captain-home' element={
+          <CaptainProtectWrapper>
+            <CaptainHome />
+          </CaptainProtectWrapper>
+
+        } />
       </Routes>
     </div>
   )
